209a02545cf2057844e7f15242c9441fe34cc1df
Vader20FF <ljaniszewski00@gmail.com>
2021-11-02 13:24:19 +0100

    - added method for loading the initial board from file - added methods for preparing solution files - added method for checking the coordinates of empty field

--- Board.py
+++ Board.py
@@ -1,7 +1,10 @@
+import time
+
+
 class Board:
     row_count = 0
     column_count = 0
-    coordinates_of_empty_field = {}
+    empty_field_coordinates = {}
     board = []
     solved_board_3_3 = [['1', '2', '3'],
                         ['4', '5', '6'],
@@ -14,12 +17,53 @@ class Board:
 
     def __init__(self, start_file_name):
         self.load_start_board(start_file_name)
+        self.set_empty_field_coordinates()
 
     def load_start_board(self, start_file_name):
         f = open(start_file_name, "r")
         lines = f.readlines()
+        print(lines)
+        row_count = lines[0][0]
+        column_count = lines[0][2]
         lines = lines[1:-1]
         for line in lines:
-            self.board.append(line)
+            formatted_line = line.rstrip().split()
+            self.board.append(formatted_line)
+
+    def create_solution_file(self, solution_file_name, solved, solution_length, moves):
+        f = open(solution_file_name, 'w+')
+        if solved:
+            f.write(solution_length)
+            f.write('\n')
+            f.write(moves)
+            f.write('\n')
+        else:
+            f.write("-1\n")
+        f.close()
+
+    def create_additional_file(self, additional_file_name, solved, solution_length, visited_nodes, processed_nodes,
+                              depth_level, processing_time):
+        f = open(additional_file_name, 'w+')
+        if solved:
+            f.write(solution_length)
+            f.write('\n')
+            f.write(visited_nodes)
+            f.write('\n')
+            f.write(processed_nodes)
+            f.write('\n')
+            f.write(depth_level)
+            f.write('\n')
+            f.write(str(round((time.time() - processing_time) * 1000, 3)))
+            f.write('\n')
+        else:
+            f.write("-1\n")
+        f.close()
+
+    def set_empty_field_coordinates(self):
+        for row in range(len(self.board)):
+            for column in range(len(self.board) + 1):
+                if self.board[row][column] == "0":
+                    self.empty_field_coordinates = {row, column}
+
 
 
--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 # SISE 2021
 # LABORATORY 1 - "FIFTEEN PUZZLE"
+from Board import Board
+
+board = Board("4x4_01_0001.txt")
+for row in board.board:
+    print(row)
+
